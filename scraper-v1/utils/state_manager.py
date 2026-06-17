dac6b2f8aacd39e21239a375abbb72349764111f
go3dc <leadership@go3dc.com>
2026-06-17 09:46:57 -0400

    Create state_manager.py
    Implements checkpointing by tracking completed tasks in a JSON file, preventing redundant work and allowing the agent to resume after crashes.

---
+++ scraper-v1/utils/state_manager.py
@@ -0,0 +1,9 @@
+import json, os
+class StateManager:
+    def __init__(self, file='state.json'):
+        self.file = file
+        self.state = json.load(open(file)) if os.path.exists(file) else []
+    def is_processed(self, zip): return zip in self.state
+    def mark_as_processed(self, zip):
+        self.state.append(zip)
+        json.dump(self.state, open(self.file, 'w'))
